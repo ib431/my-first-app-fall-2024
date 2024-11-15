f8b0ef6ec960fffe6bfb3afb18c940cd6ed005a9
ib431 <ib431@georgetown.edu>
2024-11-15 16:57:43 -0500

    updating stocks api key to not print

--- app/stocks.py
+++ app/stocks.py
@@ -8,8 +8,6 @@ from plotly.express import line
 
 from app.alpha_service import API_KEY
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
-
 
 def fetch_stocks_csv(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&outputsize=full&datatype=csv"
